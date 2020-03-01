78710da23ec4b4b2bb8bc578a43cd2cc0becf2b7
Qinghui.Liu <Qinghui.Liu@fanruan.com>
2020-03-01 13:54:26 +0800

    add VisualAccumulator

--- src/Test.java
+++ src/Test.java
@@ -1,16 +1,13 @@
 public class Test {
 
-    public static String exR2(int n) {
+    public static void main(String[] args) {
+        int T = Integer.parseInt(args[0]);
+        VisualAccumulator v = new VisualAccumulator(T, 1.0);
 
-        if (n <= 0) {
-            return "";
+        for (int i = 0; i < T; i++) {
+            v.addDataValue(StdRandom.random());
         }
 
-        return exR2(n - 3) + n + exR2(n - 2) + n;
-    }
-
-    public static void main(String[] args) {
-
-        StdOut.print(exR2(6));
+        StdOut.println(v);
     }
 }
